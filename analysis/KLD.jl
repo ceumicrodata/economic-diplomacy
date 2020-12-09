85ae2ade08b8d460dbc549b1c98fa947282feefd
Miklos Koren <miklos.koren@gmail.com>
2020-12-09 20:34:55 +0100

    bugfix: need at least 2 sectors and 2 countries to compute p values (fix #37, fix #36)

--- analysis/KLD.jl
+++ analysis/KLD.jl
@@ -14,19 +14,23 @@ function compute_KLD(share::Array{Float64,N}, p::Vector{Float64}) where {N}
     return sum(share .* deviation, dims=1)
 end
 
-function compute_p_values(A)
+function compute_p_values(A; debug=false)
+    # countries with positive shipments
+    non_empty_columns = vec(sum(A, dims=1) .> 0)
     # exclude empty rows
-    data = A[vec(sum(A, dims=2) .> 0), :]
+    data = A[vec(sum(A, dims=2) .> 0), non_empty_columns]
     K, N = size(data)
-    
+
+    if debug
+        println(K, N)
+        println(A)
+        println(data)
+    end
+
     # if no data left, return p = 1.0 because we cannot reject the null
-    K > 0 || return ones(Float64, N)
-    
-    # countries with no shipments at all
-    empty_columns = vec(sum(data, dims=1) .== 0)
-    # add 1 shipment to these so that algorithm completes but change p = 1 later
-    data[1, empty_columns] .= 1
-    
+    # cannot estimate countries where all shipments go to the same bin
+    (K > 1 && N > 1)|| return ones(Float64, N), non_empty_columns
+
     H0_params = Polya.mle(DirichletMultinomial, data, tol=1e-4)
     H0_shares = H0_params.α ./ sum(H0_params.α)
     actual_KLD = compute_KLD(data ./ sum(data, dims=1), H0_shares)
@@ -41,9 +45,8 @@ function compute_p_values(A)
             maxiter=10000, digits=3)
         p[i] = 1 - cdf(pmf, actual_KLD[1,i])
     end
-    # we have no information to reject the null
-    p[empty_columns] .= 1.0
-    return p
+    # return the p vector and an index of where to put it
+    return p, non_empty_columns
 end
 
 
@@ -58,21 +61,24 @@ end
 function main(input_file::String, output_file::String)
     data = DataFrame(load(input_file))
     years = unique(data.year)
-    destinations = unique(DataFrame(load("../temp/regression_countries.csv")).iso2_d)
 
     header = Array(data[:,1:3])
     input = Array(data[:,4:end])
     output = ones(Float64, size(input, 1))
 
-    Threads.@threads for d in destinations
-        for t in years
-            println(d, t)
+    Threads.@threads for row in eachrow(unique(data[:,2:3]))
+        d = row[1]
+        t = row[2]
+        println(d, t)
 
-            indexes = (header[:,2] .== d) .& (header[:,3] .== t)
-            subset = input[indexes, :]
-            p = compute_p_values(flip(subset))
-            output[indexes] .= p
-        end
+        indexes = (header[:,2] .== d) .& (header[:,3] .== t)
+        subset = input[indexes, :]
+        long_p_vector = ones(Float64, size(subset, 1))
+        short_p_vector, non_empty_columns = compute_p_values(flip(subset), debug=false)
+        # only use p values for countries with non-missing data, rest are 1.0
+        long_p_vector[non_empty_columns] .= short_p_vector
+        # round p-value to 4 digits
+        output[indexes] .= round.(short_p_vector*1e4) / 1e4
     end
 
     df = DataFrame(iso2_o=header[:,1], iso2_d=header[:,2], year=header[:,3], p=output)
